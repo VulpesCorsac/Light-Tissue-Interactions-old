0aabf220bebada3d075e0eecaf1b95516236d38a
George <shaxirrim@gmail.com>
2021-03-07 16:27:01 +0300

    Create main_ad.cpp

---
+++ main_ad.cpp
@@ -0,0 +1,42 @@
+// This is an open source non-commercial project. Dear PVS-Studio, please check it.
+// PVS-Studio Static Code Analyzer for C, C++, C#, and Java: http://www.viva64.com
+
+#include "AD/Quadrature.h"
+#include "AD/RT.h"
+
+#include <iostream>
+
+int main (int argc, char **argv) {
+    using T = float;
+    const int M = 4;
+
+    T a = 0.9; // albedo
+    T tau = 1.0; // optical thickness
+    T g = 0.9; // anisotropy
+    T n_slab = 1.4; // refraction index of sample
+    T n_slide_top = 1.5; // refraction index of slide
+    T n_slide_bottom = 1.5;
+
+    Quadrature<T,M> quadrature(n_slab);
+
+    const auto v = quadrature.getV();
+    const auto w = quadrature.getW();
+
+    /*
+    std::cout << "Knots" << std::endl;
+    quadrature.printQuadrature(v);
+    std::cout << "Weights" << std::endl;
+    quadrature.printQuadrature(w);
+    //*/
+
+
+    T ts, rs;
+    RTs<T,M>(a, tau, g, n_slab, n_slide_top, n_slide_bottom, v, w, rs, ts);
+    std::cout << "T scattered = " << ts << std::endl;
+    std::cout << "R scattered = " << rs << std::endl;
+
+    T tc = Tc<T,M>(tau, n_slab, n_slide_top, n_slide_bottom);
+    std::cout << "T collimated = " << tc << std::endl;
+
+    return 0;
+}
