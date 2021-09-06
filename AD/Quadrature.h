10cba11a743460ead053facf46293a4088e9a97c
George <shaxirrim@gmail.com>
2021-09-06 22:11:16 +0300

    Revert "UPD"
    This reverts commit c7c475a58152796336af2fe7590f336803b80b95.

--- AD/Quadrature.h
+++ AD/Quadrature.h
@@ -40,13 +40,13 @@ protected:
  ******************/
 
 template < typename T, size_t M >
-Quadrature<T,M>::Quadrature(T n_slab) {
+Quadrature<T,M>::Quadrature(T n_slab)  {
     setValues(n_slab);
     calculateQuadrature();
 }
 
 template < typename T, size_t M >
-void Quadrature<T,M>::setValues(const T& n_slab) {
+void Quadrature<T,M>::setValues(const T& n_slab)  {
     vc = Vc(n_slab);
 }
 
@@ -70,7 +70,7 @@ std::array<T,M> Quadrature<T,M>::getW() const  {
 }
 
 template < typename T, size_t M >
-void Quadrature<T,M>::calculateQuadrature() {
+void Quadrature<T,M>::calculateQuadrature()  {
     gaussQuadrature();
     radauQuadrature();
     mergeQuadratures();
@@ -84,7 +84,7 @@ T Quadrature<T,M>::dLegendre(int n, T x) const  {
 }
 
 template < typename T, size_t M >
-void Quadrature<T,M>::gaussQuadrature() {
+void Quadrature<T,M>::gaussQuadrature()  {
     using namespace std;
     const int n = M/2;
     for (int i = 1; i <= n; i++) {
@@ -108,7 +108,7 @@ void Quadrature<T,M>::gaussQuadrature() {
 }
 
 template < typename T, size_t M >
-void Quadrature<T,M>::radauQuadrature() {
+void Quadrature<T,M>::radauQuadrature()  {
     if (M % 2)
         throw std::invalid_argument("Quadrature should take even M as parameter");
     if (M < 4)
@@ -153,7 +153,7 @@ void Quadrature<T,M>::radauQuadrature() {
         roots[14] = -0.9714610905263484;
         roots[15] = -1.0;
     }
-    std::sort(AllContainer(roots), std::greater<T>());
+    std::sort(begin(roots), end(roots), std::greater<T>());
     /*
     for (const auto& x : roots) {
         w_r.push_back((1 - vc) / (2 * (1 - x) * sqr(dLegendre(n-1, x))));
@@ -170,7 +170,7 @@ void Quadrature<T,M>::radauQuadrature() {
 }
 
 template < typename T, size_t M >
-void Quadrature<T,M>::mergeQuadratures () {
+void Quadrature<T,M>::mergeQuadratures ()  {
     // v = v_g;
     // w = w_g;
     // v.insert(v.end(), v_r.begin(), v_r.end());
