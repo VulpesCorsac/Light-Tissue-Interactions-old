f2691ca394b24cc0d31b9d1b42b1d6544c98c3a7
George <shaxirrim@gmail.com>
2021-09-08 22:25:12 +0300

    Change PVS script

--- pvs_ADMC.sh
+++ pvs_ADMC.sh
@@ -1,3 +1,3 @@
-pvs-studio-analyzer trace -- g++ -std=c++17 -O2 main_mc.cpp -o main_mc -pthread
+pvs-studio-analyzer trace -- g++ -std=c++17 -O2 main_admc.cpp -o main_admc -pthread
 pvs-studio-analyzer analyze -o log
 plog-converter -a GA:1,2,3 -t fullhtml -o tasks log
