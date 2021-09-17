bba4e953bbeac1c767ba5b6124d06d0320645810
George <shaxirrim@gmail.com>
2021-09-17 20:40:57 +0300

    Temporary remove cpp instances for templates

--- Physics/BugerLambert.cpp
+++
@@ -1,4 +0,0 @@
-#include "BugerLambert.h"
-
-template float  Physics_NS::BugerLambert<float> (float , float , float , float );
-template double Physics_NS::BugerLambert<double>(double, double, double, double);
--- Physics/Reflectance.cpp
+++
@@ -1,7 +0,0 @@
-#include "Reflectance.h"
-
-template float  Physics_NS::FresnelReflectance<float> (float , float , float );
-template double Physics_NS::FresnelReflectance<double>(double, double, double);
-
-template float  Physics_NS::BorderReflectance<float> (float , float );
-template double Physics_NS::BorderReflectance<double>(double, double);
