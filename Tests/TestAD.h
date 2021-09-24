871b8b1267995ea89b6cb228b6022e9eeec3e03e
George <shaxirrim@gmail.com>
2021-09-24 21:50:16 +0300

    Fix thresholds for appveyor

--- Tests/TestAD.h
+++ Tests/TestAD.h
@@ -52,7 +52,7 @@ void test1() {
     EXPECT_NEAR(test.getTc(), 0.338341, TOLERANCE);
 }
 
-TEST_AND_BENCHMARK(AD, Test1, test1, 100, 700)
+TEST_AND_BENCHMARK(AD, Test1, test1, 100, 2200)
 
 void test2() {
     testDataRT<float,16> test(0.9, 2.0, 0.99, 1.5, 1.5, 1.5);
@@ -61,7 +61,7 @@ void test2() {
     EXPECT_NEAR(test.getTc(), 0.124729, TOLERANCE);
 }
 
-TEST_AND_BENCHMARK(AD, Test2, test2, 100, 100)
+TEST_AND_BENCHMARK(AD, Test2, test2, 100, 300)
 
 void g0() {
     testDataRT<double,4> test(0.95, 5.0, 0.0, 1.4, 1.4, 1.4);
@@ -80,7 +80,7 @@ void a0() {
     EXPECT_NEAR(test.getTc(), test.getTs(), TOLERANCE_T);
 }
 
-TEST_AND_BENCHMARK(AD, A0, a0, 1000, 150)
+TEST_AND_BENCHMARK(AD, A0, a0, 1000, 350)
 
 void rtTestA0G0() {
     testDataRT<double,32> test(0.0, 1.0, 0.0, 1.3, 1.4, 1.4);
@@ -90,4 +90,4 @@ void rtTestA0G0() {
     EXPECT_NEAR(test.getTc(), test.getTs(), TOLERANCE_T);
 }
 
-TEST_AND_BENCHMARK(AD, RTtestA0G0, rtTestA0G0, 100, 150)
+TEST_AND_BENCHMARK(AD, RTtestA0G0, rtTestA0G0, 100, 200)
