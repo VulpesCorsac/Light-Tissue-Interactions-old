2c14745cb83448c466be203fce60f500465aa3e7
George <shaxirrim@gmail.com>
2021-10-16 22:37:47 +0300

    Add detector utils and some tests for them

--- MC/Detectors/DetectorUtils.h
+++ MC/Detectors/DetectorUtils.h
@@ -1,5 +1,169 @@
 #pragma once
 
-namespace MonteCarlo_NS {
+#ifdef ASSERT_INPUT_PARAMS
+    #define EXCEPT_INPUT_PARAMS
+#else
+    #define EXCEPT_INPUT_PARAMS noexcept
+#endif // ASSERT_INPUT_PARAMS
 
+#include "DetectorInterface.h"
+#include "DetectorProperties.h"
+#include "DetectorType.h"
+#include "DistancesSweep.h"
+#include "FullAbsorber.h"
+#include "IntegratingSphereComplex.h"
+#include "IntegratingSphereSimple.h"
+#include "OpticalFiber.h"
+
+#include <algorithm>
+#include <stdexcept>
+#include <string>
+
+namespace MonteCarlo_NS {
+    /// Detector type getter from DetectorInterface pointer
+    /// \param[in] detector DetectorInterface pointer
+    /// \return DetectorType, DetectorType::Unknown if cannot convert to the known types
+    /// \throw std::invalid_argument If ASSERT_INPUT_PARAMS is defined and cannot convert to the known types
+    template < typename T >
+    inline DetectorType detectorType(DetectorInterface<T>* const detector) EXCEPT_INPUT_PARAMS;
+
+    /// Detector type getter from DetectorInterface pointer
+    /// \param[in] detector Detector name
+    /// \return DetectorType, DetectorType::Unknown if cannot convert to the known types
+    /// \throw std::invalid_argument If ASSERT_INPUT_PARAMS is defined and cannot convert to the known types
+    inline DetectorType detectorType(const std::string& detector) EXCEPT_INPUT_PARAMS;
+
+    /// Validator for DetectorProperties
+    /// \param[in] properties DetectorProperties
+    /// \return in case validation succeeded no exception or value is returned
+    /// \throw std::logic_error if not all needed properties are available
+    template < typename T >
+    void validate(const DetectorProperties<T>& properties);
+    /// Validator for DetectorProperties, calls validate
+    /// \param[in] properties DetectorProperties
+    /// \return true if validation succeeded no exception or false otherwise
+    template < typename T >
+    bool validateSafe(const DetectorProperties<T>& properties) noexcept;
+
+    /// Export detector properties to DetectorProperties
+    /// \param[in] detector DetectorInterface pointer
+    /// \return DetectorProperties for corresponding detector
+    template < typename T >
+    DetectorProperties<T> exportDetectorProperties(DetectorInterface<T>* const detector) noexcept;
+}
+
+/******************
+ * IMPLEMENTATION *
+ ******************/
+
+template < typename T >
+MonteCarlo_NS::DetectorType MonteCarlo_NS::detectorType(MonteCarlo_NS::DetectorInterface<T>* const detector) EXCEPT_INPUT_PARAMS {
+    if (dynamic_cast<MonteCarlo_NS::FullAbsorber<T>*>(detector))
+        return MonteCarlo_NS::DetectorType::FullAbsorber;
+    if (dynamic_cast<MonteCarlo_NS::IntegratingSphereSimple<T>*>(detector))
+        return MonteCarlo_NS::DetectorType::IntegratingSphereSimple;
+    if (dynamic_cast<MonteCarlo_NS::IntegratingSphereComplex<T>*>(detector))
+        return MonteCarlo_NS::DetectorType::IntegratingSphereComplex;
+    if (dynamic_cast<MonteCarlo_NS::OpticalFiber<T>*>(detector))
+        return MonteCarlo_NS::DetectorType::OpticalFiber;
+
+    #ifdef ASSERT_INPUT_PARAMS
+        throw std::invalid_argument("Detector type cannot be evaluated");
+    #endif // ASSERT_INPUT_PARAMS
+
+    return MonteCarlo_NS::DetectorType::Unknown;
+}
+
+MonteCarlo_NS::DetectorType MonteCarlo_NS::detectorType(const std::string& detector) EXCEPT_INPUT_PARAMS {
+    auto lower = detector;
+    std::transform(lower.begin(), lower.end(), lower.begin(), [](unsigned char c) { return std::tolower(c); } );
+
+    if (lower == "fullabsorber"  ||
+        lower == "full absorber" ||
+        lower == "full_absorber" ||
+        lower == "full-absorber")
+        return MonteCarlo_NS::DetectorType::FullAbsorber;
+    if (lower == "integratingspheresimple"   ||
+        lower == "integrating sphere simple" ||
+        lower == "integrating_sphere_simple" ||
+        lower == "integrating-sphere-simple")
+        return MonteCarlo_NS::DetectorType::IntegratingSphereSimple;
+    if (lower == "integratingspherecomplex"   ||
+        lower == "integrating sphere complex" ||
+        lower == "integrating_sphere_complex" ||
+        lower == "integrating-sphere-complex")
+        return MonteCarlo_NS::DetectorType::IntegratingSphereComplex;
+    if (lower == "opticalfiber"  ||
+        lower == "optical fiber" ||
+        lower == "optical_fiber" ||
+        lower == "optical-fiber")
+        return MonteCarlo_NS::DetectorType::OpticalFiber;
+
+    #ifdef ASSERT_INPUT_PARAMS
+        throw std::invalid_argument("Medium type cannot be evaluated");
+    #endif // ASSERT_INPUT_PARAMS
+
+    return MonteCarlo_NS::DetectorType::Unknown;
+}
+
+template < typename T >
+void MonteCarlo_NS::validate(const MonteCarlo_NS::DetectorProperties<T>& properties) {
+    if (properties.type == MonteCarlo_NS::DetectorType::Unknown)
+        throw std::logic_error("Validation fails for DetectorType::Unknown");
+
+    if (properties.type == MonteCarlo_NS::DetectorType::FullAbsorber) {
+        if (!properties.collimatedCosine.has_value())
+            throw std::logic_error("Validation fails for DetectorType::FullAbsorber because there is no collimatedCosine property");
+        if (properties.collimatedCosine.value() < -1)
+            throw std::logic_error("Validation fails for DetectorType::FullAbsorber because collimatedCosine property is less than 0");
+        if (properties.collimatedCosine.value() > +1)
+            throw std::logic_error("Validation fails for DetectorType::FullAbsorber because collimatedCosine property is greater than +1");
+    }
+
+    if (properties.type == MonteCarlo_NS::DetectorType::IntegratingSphereSimple) {
+
+    }
+
+    if (properties.type == MonteCarlo_NS::DetectorType::IntegratingSphereComplex) {
+
+    }
+
+    if (properties.type == MonteCarlo_NS::DetectorType::OpticalFiber) {
+
+    }
+}
+
+template < typename T >
+bool MonteCarlo_NS::validateSafe(const MonteCarlo_NS::DetectorProperties<T>& properties) noexcept {
+    try {
+        validate(properties);
+        return true;
+    } catch(std::logic_error&) {
+        return false;
+    }
+}
+
+template < typename T >
+MonteCarlo_NS::DetectorProperties<T> MonteCarlo_NS::exportDetectorProperties(MonteCarlo_NS::DetectorInterface<T>* const detector) noexcept {
+    MonteCarlo_NS::DetectorProperties<T> result;
+    result.type = detectorType(detector);
+
+    if (result.type == MonteCarlo_NS::DetectorType::FullAbsorber) {
+        auto casted_detector = dynamic_cast<MonteCarlo_NS::FullAbsorber<T>*>(detector);
+        result.collimatedCosine = casted_detector.collimatedCosine;
+    }
+
+    if (result.type == MonteCarlo_NS::DetectorType::IntegratingSphereSimple) {
+        auto casted_detector = dynamic_cast<MonteCarlo_NS::IntegratingSphereSimple<T>*>(detector);
+    }
+
+    if (result.type == MonteCarlo_NS::DetectorType::IntegratingSphereComplex) {
+        auto casted_detector = dynamic_cast<MonteCarlo_NS::IntegratingSphereComplex<T>*>(detector);
+    }
+
+    if (result.type == MonteCarlo_NS::DetectorType::OpticalFiber) {
+        auto casted_detector = dynamic_cast<MonteCarlo_NS::OpticalFiber<T>*>(detector);
+    }
+
+    return result;
 }
--- MC/Detectors/DetectorUtilsTests.h
+++ MC/Detectors/DetectorUtilsTests.h
@@ -4,4 +4,124 @@
     #define ASSERT_INPUT_PARAMS
 #endif // ASSERT_INPUT_PARAMS
 
+#include "DetectorUtils.h"
+
 #include <gtest/gtest.h>
+
+using namespace MonteCarlo_NS;
+
+TEST(DetectorUtilsTests, DetectorTypeFromPtr_ThrownExceptionForNullptr) {
+    EXPECT_THROW(detectorType<float>(nullptr), std::invalid_argument);
+}
+
+TEST(DetectorUtilsTests, DetectorTypeFromPtr_ThrownExceptionForInterface) {
+    std::unique_ptr<DetectorInterface<float>> detector = std::make_unique<DetectorInterface<float>>();
+    EXPECT_THROW(detectorType<float>(detector.get()), std::invalid_argument);
+}
+
+TEST(DetectorUtilsTests, DetectorTypeFromPtr_ThrownExceptionForUnknownType) {
+    class DetectorDummy: public DetectorInterface<float> {};
+
+    auto detector = std::make_unique<DetectorDummy>();
+    EXPECT_THROW(detectorType<float>(detector.get()), std::invalid_argument);
+}
+
+TEST(DetectorUtilsTests, DetectorTypeFromPtr_DetectorFullAbsorber) {
+    auto detector = std::make_unique<FullAbsorber<float>>();
+    EXPECT_EQ(detectorType<float>(detector.get()), DetectorType::FullAbsorber);
+}
+
+TEST(DetectorUtilsTests, DetectorTypeFromPtr_DetectorIntegratingSphereSimple) {
+    auto detector = std::make_unique<IntegratingSphereSimple<float>>();
+    EXPECT_EQ(detectorType<float>(detector.get()), DetectorType::IntegratingSphereSimple);
+}
+
+TEST(DetectorUtilsTests, DetectorTypeFromPtr_DetectorIntegratingSphereComplex) {
+    auto detector = std::make_unique<IntegratingSphereComplex<float>>();
+    EXPECT_EQ(detectorType<float>(detector.get()), DetectorType::IntegratingSphereComplex);
+}
+
+TEST(DetectorUtilsTests, DetectorTypeFromPtr_DetectorOpticalFiber) {
+    auto detector = std::make_unique<OpticalFiber<float>>();
+    EXPECT_EQ(detectorType<float>(detector.get()), DetectorType::OpticalFiber);
+}
+
+TEST(DetectorUtilsTests, DetectorTypeFromStr_ThrownExceptionForEmptyStr) {
+    EXPECT_THROW(detectorType(""), std::invalid_argument);
+}
+
+TEST(DetectorUtilsTests, DetectorTypeFromStr_ThrownExceptionForUnknownType) {
+    EXPECT_THROW(detectorType("InvalidDetector"), std::invalid_argument);
+}
+
+TEST(DetectorUtilsTests, DetectorTypeFromStr_DetectorFullAbsorber) {
+    EXPECT_EQ(detectorType("fullabsorber" ), DetectorType::FullAbsorber);
+    EXPECT_EQ(detectorType("FullAbsorber" ), DetectorType::FullAbsorber);
+    EXPECT_EQ(detectorType("FULLABSORBER" ), DetectorType::FullAbsorber);
+    EXPECT_EQ(detectorType("full absorber"), DetectorType::FullAbsorber);
+    EXPECT_EQ(detectorType("Full Absorber"), DetectorType::FullAbsorber);
+    EXPECT_EQ(detectorType("FULL ABSORBER"), DetectorType::FullAbsorber);
+    EXPECT_EQ(detectorType("full_absorber"), DetectorType::FullAbsorber);
+    EXPECT_EQ(detectorType("Full_Absorber"), DetectorType::FullAbsorber);
+    EXPECT_EQ(detectorType("FULL_ABSORBER"), DetectorType::FullAbsorber);
+    EXPECT_EQ(detectorType("full-absorber"), DetectorType::FullAbsorber);
+    EXPECT_EQ(detectorType("Full-Absorber"), DetectorType::FullAbsorber);
+    EXPECT_EQ(detectorType("FULL-ABSORBER"), DetectorType::FullAbsorber);
+}
+
+TEST(DetectorUtilsTests, DetectorTypeFromStr_DetectorIntegratingSphereSimple) {
+    EXPECT_EQ(detectorType("integratingspheresimple"  ), DetectorType::IntegratingSphereSimple);
+    EXPECT_EQ(detectorType("IntegratingSphereSimple"  ), DetectorType::IntegratingSphereSimple);
+    EXPECT_EQ(detectorType("INTEGRATINGSPHERESIMPLE"  ), DetectorType::IntegratingSphereSimple);
+    EXPECT_EQ(detectorType("integrating sphere simple"), DetectorType::IntegratingSphereSimple);
+    EXPECT_EQ(detectorType("Integrating Sphere Simple"), DetectorType::IntegratingSphereSimple);
+    EXPECT_EQ(detectorType("INTEGRATING SPHERE SIMPLE"), DetectorType::IntegratingSphereSimple);
+    EXPECT_EQ(detectorType("integrating_sphere_simple"), DetectorType::IntegratingSphereSimple);
+    EXPECT_EQ(detectorType("Integrating_Sphere_Simple"), DetectorType::IntegratingSphereSimple);
+    EXPECT_EQ(detectorType("INTEGRATING_SPHERE_SIMPLE"), DetectorType::IntegratingSphereSimple);
+    EXPECT_EQ(detectorType("integrating-sphere-simple"), DetectorType::IntegratingSphereSimple);
+    EXPECT_EQ(detectorType("Integrating-Sphere-Simple"), DetectorType::IntegratingSphereSimple);
+    EXPECT_EQ(detectorType("INTEGRATING-SPHERE-SIMPLE"), DetectorType::IntegratingSphereSimple);
+}
+
+TEST(DetectorUtilsTests, DetectorTypeFromStr_DetectorIntegratingSphereComplex) {
+    EXPECT_EQ(detectorType("integratingspherecomplex"  ), DetectorType::IntegratingSphereComplex);
+    EXPECT_EQ(detectorType("IntegratingSphereComplex"  ), DetectorType::IntegratingSphereComplex);
+    EXPECT_EQ(detectorType("INTEGRATINGSPHERECOMPLEX"  ), DetectorType::IntegratingSphereComplex);
+    EXPECT_EQ(detectorType("integrating sphere complex"), DetectorType::IntegratingSphereComplex);
+    EXPECT_EQ(detectorType("Integrating Sphere Complex"), DetectorType::IntegratingSphereComplex);
+    EXPECT_EQ(detectorType("INTEGRATING SPHERE COMPLEX"), DetectorType::IntegratingSphereComplex);
+    EXPECT_EQ(detectorType("integrating_sphere_complex"), DetectorType::IntegratingSphereComplex);
+    EXPECT_EQ(detectorType("Integrating_Sphere_Complex"), DetectorType::IntegratingSphereComplex);
+    EXPECT_EQ(detectorType("INTEGRATING_SPHERE_COMPLEX"), DetectorType::IntegratingSphereComplex);
+    EXPECT_EQ(detectorType("integrating-sphere-complex"), DetectorType::IntegratingSphereComplex);
+    EXPECT_EQ(detectorType("Integrating-Sphere-Complex"), DetectorType::IntegratingSphereComplex);
+    EXPECT_EQ(detectorType("INTEGRATING-SPHERE-COMPLEX"), DetectorType::IntegratingSphereComplex);
+}
+
+TEST(DetectorUtilsTests, DetectorTypeFromStr_DetectorOpticalFiber) {
+    EXPECT_EQ(detectorType("opticalfiber" ), DetectorType::OpticalFiber);
+    EXPECT_EQ(detectorType("OpticalFiber" ), DetectorType::OpticalFiber);
+    EXPECT_EQ(detectorType("OPTICALFIBER" ), DetectorType::OpticalFiber);
+    EXPECT_EQ(detectorType("optical fiber"), DetectorType::OpticalFiber);
+    EXPECT_EQ(detectorType("Optical Fiber"), DetectorType::OpticalFiber);
+    EXPECT_EQ(detectorType("OPTICAL FIBER"), DetectorType::OpticalFiber);
+    EXPECT_EQ(detectorType("optical_fiber"), DetectorType::OpticalFiber);
+    EXPECT_EQ(detectorType("Optical_Fiber"), DetectorType::OpticalFiber);
+    EXPECT_EQ(detectorType("OPTICAL_FIBER"), DetectorType::OpticalFiber);
+    EXPECT_EQ(detectorType("optical-fiber"), DetectorType::OpticalFiber);
+    EXPECT_EQ(detectorType("Optical-Fiber"), DetectorType::OpticalFiber);
+    EXPECT_EQ(detectorType("OPTICAL-FIBER"), DetectorType::OpticalFiber);
+}
+
+TEST(DetectorUtilsTests, Validate_ThrownExceptionForUnknownType) {
+    DetectorProperties<int> properties;
+    properties.type = DetectorType::Unknown;
+    EXPECT_THROW(validate(properties), std::logic_error);
+}
+
+TEST(DetectorUtilsTests, ValidateSafe_ReturnsFalseForUnknownType) {
+    DetectorProperties<int> properties;
+    properties.type = DetectorType::Unknown;
+    EXPECT_FALSE(validateSafe(properties));
+}
