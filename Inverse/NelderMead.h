895fd3e867dd0f3342e60b023ee5839e09cdace9
Tatiana Karpova <karpova.tk@phystech.edu>
2022-04-13 07:16:04 +0300

    Update NelderMead.h

--- Inverse/NelderMead.h
+++ Inverse/NelderMead.h
@@ -96,7 +96,7 @@ template < typename T >
 T Inverse_NS::aComp2a(T aC) {
     using namespace Math_NS;
 
-    CHECK_ARGUMENT_CONTRACT(aC != 0);
+    //CHECK_ARGUMENT_CONTRACT(aC != 0);
 
     return (sqrt(sqr(aC) + 4) + aC - 2) / (2 * aC);
 }
