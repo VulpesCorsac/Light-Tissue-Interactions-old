9372a9c352ede596edf94e0924d510312dfaae24
karpova.tk@phystech.edu <karpova.tk@phystech.edu>
2022-04-18 11:18:12 +0300

    MC tests

--- MC/LightSource.h
+++ MC/LightSource.h
@@ -33,22 +33,20 @@ LightSource<T>::LightSource(const Vector3D<T>& newCentreCoord, const T& newRadiu
 
 template < typename T >
 Vector3D<T> LightSource<T>::getPhotonCoord() const noexcept {
-    using namespace Math_NS;
-
     Vector3D<T> coord;
     if (type == SourceType::Point)
         coord = this->centreCoord;
     else if (type == SourceType::Circle) {
         T RNDx, RNDy;
         do {
-            RNDx = random<T>(-1, 1);
-            RNDy = random<T>(-1, 1);
-        } while ((sqr(RNDx) + sqr(RNDy)) > 1);
+            RNDx = Math_NS::random<T>(-1, 1);
+            RNDy = Math_NS::random<T>(-1, 1);
+        } while ((Math_NS::sqr(RNDx) + Math_NS::sqr(RNDy)) > 1);
         coord = Vector3D<T>(sqrt(RNDx) * radius, sqrt(RNDy) * radius, 0);
     } else if (type == SourceType::Gaussian) {
         T RNDr, RNDa;
-        RNDr = random<T>(0, 1);
-        RNDa = random<T>(0,1) * 2 * M_PI;
+        RNDr = Math_NS::random<T>(0, 1);
+        RNDa = Math_NS::random<T>(0,1) * 2 * M_PI;
         coord = Vector3D<T>(sqrt(-log(RNDr)) * radius * cos(RNDa), sqrt(-log(RNDr)) * radius * sin(RNDa), 0);
     }
     return coord;
